e2ee4cfe91ada0abb5c0bb907fd24ebd3df3c42f
Ted <62585822+TedHigh@users.noreply.github.com>
2026-02-18 04:46:16 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -93,7 +93,7 @@ var l_musicRandom = false;
 
 /*Music volume
 Choose a value between 0 and 100*/
-var l_musicVolume = 9;
+var l_musicVolume = 5;
 
 /*Enable custom messages?*/
 var l_messagesEnabled = true;
