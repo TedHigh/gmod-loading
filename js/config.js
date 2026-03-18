438d8fe54e5543b198df4b36fd13c84cfdd562fc
Ted <62585822+TedHigh@users.noreply.github.com>
2026-03-18 04:52:47 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -51,7 +51,7 @@ var l_background = "backgrounds/videos/dancingstars.webm";
 Place one or more images
 Multiple images will automatically shuffle between them*/
 var l_bgImages = [
-	"backgrounds/images/vice.jpg"
+	"backgrounds/images/vice.png"
 
 ];
 
